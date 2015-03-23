82a474d437305a9af932b60c687f6d788bba0831
VoxWave <victor_bankowski@hotmail.com>
2015-03-23 13:45:14 +0200

    LibGDX project generated

---
+++ DankFishing/desktop/src/com/unknownpotato/dankfishing/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.unknownpotato.dankfishing.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.unknownpotato.dankfishing.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
