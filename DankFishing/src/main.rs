e49954353a6e4da86778ad845f64f169afa20694
VoxWave <victor_bankowski@hotmail.com>
2015-03-25 14:57:09 +0200

    basic rust code done. Something slightly more complicated compared to hello world but nothing related to the game

--- DankFishing/src/main.rs
+++ DankFishing/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-	println!("Hello, world!");
+	let poopoo = 12;
+	let mut paapaa = 11;
+	println!("so the value of this integer I call poopoo is: {} and paapaa is: {}", poopoo, paapaa);
+
+	paapaa++
+	println!("now the values are: {} and {}", poopoo, paapaa);
 }
