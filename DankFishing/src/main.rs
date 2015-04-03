8b83c0c8d81a9338ff118807c3c42aeb6effb371
VoxWave <victor_bankowski@hotmail.com>
2015-04-04 00:18:34 +0300

    trying to use sdl only on rust(instead of piston) in hopes that I wont be so hellish to build.

--- DankFishing/src/main.rs
+++ DankFishing/src/main.rs
@@ -1,48 +1,3 @@
-extern crate piston;
-extern crate graphics;
-extern crate sdl2_window;
-extern crate opengl_graphics;
-
-use std::rc::Rc;
-use std::cell::RefCell;
-use piston::window::WindowSettings;
-use piston::event::{ events, RenderArgs, RenderEvent, UpdateArgs,UpdateEvent };
-
-use graphics::{ Context, rectangle, RelativeTransform };
-use sdl2_window::Sdl2Window as Window;
-use opengl_graphics::{ GlGraphics, OpenGL };
-
-pub struct App {
-	gl: GlGraphics,
-	rotation: f64
-}
-impl App {
-	fn render(&mut self, args: &RenderArgs) {
-		const DARKGREEN: [f32; 4] = [0.0, 0.5, 0.0, 1.0];
-		const BLUE: [f32; 4] = [0.0, 0.0, 1.0, 1.0];
-
-		let context = &Context::abs(args.width as f64, args.height as f64);
-
-		let center_context = &context.trans((args.width/2) as f64, (args.height / 2) as f64)
-									 .rot_rad(self.rotation)
-									 .trans(-25.0, -25.0);
-		let square = rectangle::square(0.0, 0.0, 50.0);
-
-		self.gl.draw([0, 0, args.width as i32, args.height as i32], |_, gl| {
-			graphics::clear(DARKGREEN, gl);
-
-			graphics::rectangle(BLUE, square, center_context.transform, gl);
-		});
-
-	}
-
-	fn update(&mut self, args: &UpdateArgs) {
-		self.rotation += 2.0 * args.dt;
-	}
-}
-
-
-
 fn main() {
 	let ayy = 12;
 	let mut lmao = 11;
@@ -51,25 +6,5 @@ fn main() {
 	lmao += 1;
 	println!("now the values are: {} and {}", ayy, lmao);
 
-	println!("Oh btw I'm starting the awesomepart now");
-	let window = Window::new(
-		OpenGL::_3_2,
-		WindowSettings::default()
-	);
-	let window = Rc::new(RefCell::new(window));
-
-	let mut app = App{
-		gl: GlGraphics::new(OpenGL::_3_2),
-		rotation: 0.0
-	};
-
-	for e in events(window) {
-		if let Some(r) = e.render_args() {
-			app.render(&r);
-		}
-
-		if let Some(u) = e.update_args() {
-			app.update(&u);
-		}
-	}
+	println!("Oh btw I'm starting the awesomepart now...");
 }
