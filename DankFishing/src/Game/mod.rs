6f1a9b093281b82b67326731eb6ef83e8417173a
VoxWave <victor_bankowski@hotmail.com>
2015-04-15 13:16:07 +0300

    this doesn't work. some problem with lifetimes

--- DankFishing/src/Game/mod.rs
+++ DankFishing/src/Game/mod.rs
@@ -1,16 +1,44 @@
 use sdl2::sdl::Sdl;
 use sdl2::keycode::KeyCode;
+use sdl2::video::{Window, WindowPos, OPENGL};
+use sdl2::render::{RenderDriverIndex, ACCELERATED, Renderer};
+use sdl2::pixels::Color;
 
-struct Game {
-	window: Window,
-	renderer: Renderer,
+//struct Game {
+//	window: Window,
+//	renderer: Renderer,
+//}
+
+pub fn create_window(sdl_context: Sdl) -> Window {
+	match Window::new(&sdl_context, "DankFishing", WindowPos::PosCentered, WindowPos::PosCentered, 800, 600, OPENGL) {
+		Ok(window) => window,
+		Err(err) => panic!("failed to create window: {}", err)
+	};
+}
+
+pub fn create_renderer(window: Window) -> Renderer {
+	match Renderer::from_window(window, RenderDriverIndex::Auto, ACCELERATED) {
+		Ok(renderer) => renderer,
+		Err(err) => panic!("failed to create renderer: {}", err)
+	};
 }
 
 pub fn loop_game(sdl_context: Sdl) {
+	let window = create_window(sdl_context);
+
+	//this might fuck things up if I use window in the future.
+	//lets hope that doesnt happen.
+	let mut renderer = create_renderer(window);
+
+	let mut drawer = renderer.drawer();
+
 	let mut running = true;
 	let mut event_pump = sdl_context.event_pump();
 
 	while running {
+		drawer.set_draw_color(Color::RGB(0,0,0));
+		drawer.clear();
+		drawer.present();
 		for event in event_pump.poll_iter() {
 			use sdl2::event::Event;
 
--- DankFishing/src/main.rs
+++ DankFishing/src/main.rs
@@ -2,7 +2,7 @@ extern crate sdl2;
 
 use sdl2::video::{Window, WindowPos, OPENGL};
 use sdl2::render::{RenderDriverIndex, ACCELERATED, Renderer};
-use sdl2::pixels::Color;
+use sdl2::keycode::KeyCode;
 
 mod Game;
 
@@ -10,21 +10,6 @@ fn main() {
 	println!("Starting the Dankest Fishing game ever...");
 
 	let sdl_context = sdl2::init(sdl2::INIT_EVERYTHING).unwrap();
-
-	let window = match Window::new(&sdl_context, "DankFishing", WindowPos::PosCentered, WindowPos::PosCentered, 800, 600, OPENGL) {
-		Ok(window) => window,
-		Err(err) => panic!("failed to create window: {}", err)
-	};
-
-	let mut renderer = match Renderer::from_window(window, RenderDriverIndex::Auto, ACCELERATED) {
-		Ok(renderer) => renderer,
-		Err(err) => panic!("failed to create renderer: {}", err)
-	};
-
-	let mut drawer = renderer.drawer();
-	drawer.set_draw_color(Color::RGB(0,155,0));
-	drawer.clear();
-	drawer.present();
 
 	Game::loop_game(sdl_context);
 }
