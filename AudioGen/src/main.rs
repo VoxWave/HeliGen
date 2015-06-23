154e117c28c3a946905907fc8df6d903e75632da
VoxWave <victor_bankowski@hotmail.com>
2015-06-24 01:12:26 +0300

    some file opening code written. code compiles but it panics.

--- AudioGen/src/main.rs
+++ AudioGen/src/main.rs
@@ -1,5 +1,20 @@
-use std::io:*;
+use std::path::Path;
+use std::io::BufWriter;
+use std::fs::OpenOptions;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Creating a sine wave!");
+
+    let mut options = OpenOptions::new();
+
+    options.write(true).append(true);
+
+    let path = Path::new("sine.wav");
+
+    let file = match options.open(&path) {
+        Ok(file) => file,
+        Err(..) => panic!("error opening a file"),
+    };
+
+    let mut writer = BufWriter::new(&file);
 }
