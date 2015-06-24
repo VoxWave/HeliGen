e58458b005d3b8ba1c8e4ef45ebbf7eac07b9898
VoxWave <victor_bankowski@hotmail.com>
2015-06-25 01:20:00 +0300

    added sine.wav to the gitignore

--- AudioGen/src/main.rs
+++ AudioGen/src/main.rs
@@ -26,6 +26,8 @@ use std::io::BufWriter;
 use std::fs::OpenOptions;
 
 fn main() {
+
+
     println!("Generating a sine wave to sine.wav");
 
     let mut options = OpenOptions::new();
@@ -34,12 +36,12 @@ fn main() {
 
     let path = Path::new("sine.wav");
 
-    let mut file = match options.open(&path) {
+    let file = match options.open(&path) {
         Ok(file) => file,
         Err(..) => panic!("error opening a file"),
     };
 
     let mut writer = BufWriter::new(&file);
 
-    writer.write_all(b"pingas\n");
+    writer.write_all(b"RIFF");
 }
