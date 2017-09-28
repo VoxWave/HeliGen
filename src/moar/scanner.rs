76830fe9cb1e5cba87f2bcc40da9dd346f565ddd
VoxWave <victor_bankowski@hotmail.com>
2017-09-28 19:28:09 +0300

    iterated ideas on the syntax

--- src/moar/scanner.rs
+++ src/moar/scanner.rs
@@ -1,12 +1,18 @@
-struct Scanner {
+pub struct Scanner {
     source: String,
     tokens: Vec<Token>,
     scanner_state: ScannerState,
     buffer_string: String,
 }
 
-enum ScannerState {
+impl Scanner {
+    pub fn new() -> Self {
+        
+    }
+}
 
+enum ScannerState {
+    Neutral,
 }
 
 enum Token {
