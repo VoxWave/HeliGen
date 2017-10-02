87056aaa2f096687b66f4ef52f37358aa4990169
Victor Bankowski <victor_bankowski@hotmail.com>
2017-10-02 23:10:39 +0300

    some progress on the scanner implementation. dyon scripts removed

--- src/moar/scanner.rs
+++ src/moar/scanner.rs
@@ -6,9 +6,29 @@ pub struct Scanner {
 }
 
 impl Scanner {
-    pub fn new() -> Self {
-        
+    pub fn new(source: String) -> Self {
+        Scanner{
+            source,
+            tokens: Vec::new(),
+            scanner_state: ScannerState::Neutral,
+            buffer_string: String::new();
+        }
     }
+
+    pub fn scan(&mut self) -> Vec<Token> {
+        for c in source.chars() {
+            match scanner_state {
+                Neutral => self.neutral_scan(&c),
+            }
+        }
+        tokens.clone()
+    }
+
+    fn neutral_scan(&mut self, c: &char) {
+        match *c {
+            '{' => tokens.push
+        }
+    } 
 }
 
 enum ScannerState {
@@ -16,5 +36,8 @@ enum ScannerState {
 }
 
 enum Token {
-
+    CurlyBracket(Direction),
+    Bracket(Direction),
+    Identifier(String),
+    
 }
